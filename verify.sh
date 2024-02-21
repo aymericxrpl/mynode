ca83726a301f17147759c4704274de7de82ee705
aymericxrpl <160792563+aymericxrpl@users.noreply.github.com>
2024-02-21 23:27:46 +0100

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -1,1 +1,1 @@
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AeaYSHD3ztHAAFj7DFxwLNbr-d-9pxiqsY2kuI3lj2o-RsLU5g3TWXhnTgiyALoztnr01Q" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AeaYSHDzmt3001IKNTPFk9d3Sa0aaxTpZM4AHFp9H8SUJMnxWSReb11P6HbD88hF8BCXfA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
